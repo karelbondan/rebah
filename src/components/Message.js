eee39f4ce7bd79625bed6d8ca98cfe14a5847748
karelbondan <karelbondan@gmail.com>
2022-06-03 04:33:38 +0700

    added component

--- src/components/Message.js
+++ src/components/Message.js
@@ -10,7 +10,7 @@ const Message = ({ handleSubmit, text, setText, setImg, img }) => {
             let files = Array.from(input.files)[0];
             setImg(files)
             console.log(files)
-            console.log(files.size * 2**-10)
+            console.log(files.size * 2 ** -10)
         };
         input.click();
     }
@@ -32,19 +32,21 @@ const Message = ({ handleSubmit, text, setText, setImg, img }) => {
                     {`Attached: ${img.name}`}
                 </h3>
             </div>
-            <form className='flex space-x-5 justify-center'>
+            <div className='flex space-x-5 w-full'>
                 <button className='buton text-gray-500' onClick={uploadIMG}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
                     </svg>
                 </button>
-                <input className='w-full rounded-xl px-3 py-2 focus:outline-none bg-gray-700 text-gray-300' type='text' placeholder='Enter message' value={text} onChange={(e) => { setText(e.target.value) }}></input>
-                <button className='text-gray-500' onClick={handleSubmit}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-                    </svg>
-                </button>
-            </form>
+                <form className='flex space-x-5 justify-center w-full'>
+                    <input className='w-full rounded-xl px-3 py-2 focus:outline-none bg-gray-700 text-gray-300' type='text' placeholder='Enter message' value={text} onChange={(e) => { setText(e.target.value) }}></input>
+                    <button className='text-gray-500' onClick={handleSubmit}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                        </svg>
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { auth, db } from '../firebase'
 import { signOut, onAuthStateChanged } from 'firebase/auth'
-import { updateDoc, doc } from 'firebase/firestore'
+import { updateDoc, doc, Timestamp } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
@@ -10,7 +10,8 @@ const Navbar = () => {
     const navigate = useNavigate();
     const handleLogout = async () => {
         await updateDoc(doc(db, 'users', auth.currentUser.uid), {
-            isOnline: false
+            isOnline: false,
+            lastSeen: Timestamp.fromDate(new Date())
         });
         await signOut(auth);
         console.log('logged out');
@@ -22,11 +23,11 @@ const Navbar = () => {
     })
 
     return (
-        <nav className='flex items-center justify-between h-16 x-screen px-20 border-b'>
-            <h3>
+        <nav className='flex items-center justify-between h-16 x-screen px-20 z-50 bg-slate-900 text-white'>
+            <h3 className='z-50'>
                 <Link to="/">Messenger</Link>
             </h3>
-            <div>
+            <div className='z-50'>
                 {userExist ?
                     <div className='space-x-2'>
                         <Link to={"/profile"}>Profile</Link>
