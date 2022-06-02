d04673629e3829f6b655140fb00bf675e6743f2c
karelbondan <karelbondan@gmail.com>
2022-06-03 04:40:29 +0700

    for testing purposes

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import defbg from '../components/items/bg3.jpg'
 
 const Test = () => {
     // let a = "sadakjdlksajd"
@@ -14,10 +15,30 @@ const Test = () => {
     });
     console.log(a)
 
-    
+
     return (
-        <div>
-            <div className='flex w-screen h-screen'>
+        // <div className='bg-gray-500 text-white flex items-center justify-center'>
+        //     test
+        // </div>
+        <div className='overflow-hidden z-0'>
+            <div className='z-50 flex flex-col items-center justify-center overflow-auto bg-gray-900 h-screen w-screen'>
+                <h3 className='z-50 loading_dots text-white text-4xl'>
+                    ••••••
+                </h3>
+                {/* <h3 className='text-white text-4xl'>
+                    Loading
+                </h3> */}
+            </div>
+            <div className='w-0 h-screen fixed top-0 left-0 z-0'>
+                <img className='opacity-30 min-h-max min-w-max ' src={defbg}></img>
+            </div>
+        </div>
+    )
+}
+
+export default Test
+
+{/* <div className='flex w-screen h-screen'>
                 <div className='bg-gray-500 border border-red-500 text-white px-2 py-2'>
                     <div className='truncate'>
                         <p>aslkdjaskdjkadklsadkjaskdjaslkdjlkasdklakldjakldklasjdkkajdklajdklaskdjkaslldkl</p>
@@ -35,9 +56,4 @@ const Test = () => {
                         Send message area
                     </div>
                 </div>
-            </div>
-        </div>
-    )
-}
-
-export default Test
+            </div> */}
