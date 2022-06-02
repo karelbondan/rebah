8e82a07dcda4d06e817bbb78ad5f361c4b1b9bf3
karelbondan <karelbondan@gmail.com>
2022-06-03 04:39:54 +0700

    adjusted background image to fill the entire screen

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -10,6 +10,7 @@ import nodeflux_original from '../components/items/nodeflux_logo.png'
 import nodeflux_white from '../components/items/nodeflux_logo_white.png'
 
 const Profile = () => {
+    const [loading, setLoading] = useState(false)
     const [pic, setPic] = useState("");
     const [user, setUser] = useState();
     const [edit, setEdit] = useState(false);
@@ -39,6 +40,7 @@ const Profile = () => {
     }
 
     const delIMG = async () => {
+        setLoading(true)
         try {
             const confirm = window.confirm("delete avatar?")
             if (confirm) {
@@ -52,6 +54,7 @@ const Profile = () => {
         } catch (galat) {
             console.log(galat.message)
         }
+        setLoading(false)
     }
 
     const handleLogout = async () => {
@@ -99,6 +102,7 @@ const Profile = () => {
         updatelocal()
         if (pic) {
             const upl = async () => {
+                setLoading(true)
                 const imgref = ref(storage, `avatar/${new Date().getTime()} - ${pic.name}`)
                 try {
                     if (user.avatarPath) {
@@ -116,40 +120,35 @@ const Profile = () => {
                 } catch (galat) {
                     console.log(galat.message)
                 }
+                setLoading(false)
             }
             upl()
         }
     }, [pic])
     return user ? (
         <div className='overflow-hidden'>
-            <div className='w-0 h-screen z-0 fixed'>
-                <img className='opacity-30 left-0 top-0 min-h-max min-w-max' src={defbg}></img>
+            <div className='w-0 h-screen left-0 top-0 z-0 fixed'>
+                <img className='opacity-30 min-h-max min-w-max' src={defbg}></img>
             </div>
-            <div className='flex items-center justify-center overflow-auto  bg-gray-900 '>
+            <div className='flex items-center justify-center overflow-auto bg-gray-900 '>
                 <div className='flex text-white h-screen w-screen justify-center items-center'>
                     <div className='z-50 bg-gray-700 px-10 py-10 space-y-6 rounded-lg shadow-xl'>
                         <div className='flex space-x-6'>
                             <div>
                                 <img src={user.avatar ? user.avatar : img} alt='spyro' className='rounded-full w-80 h-80'></img>
                                 <div className='flex justify-center mt-4 space-x-4'>
-                                    <button title="Upload avatar" className='px-8 py-3 rounded-full hover:bg-green-700 bg-gray-600 transition-all focus:outline-none' onClick={uploadIMG}>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            className="h-6 w-6"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                            strokeWidth={2}>
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+                                    <button title="Upload avatar" className={`px-8 py-3 rounded-full hover:bg-green-700 bg-gray-600 transition-all focus:outline-none ${loading ? "hidden" : ""}`} onClick={uploadIMG}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                                         </svg>
                                     </button>
-                                    <button title="Delete avatar" className='px-8 py-3 rounded-full hover:bg-red-700 bg-gray-600 transition-all focus:outline-none' onClick={delIMG}>
+                                    <button title="Delete avatar" className={`px-8 py-3 rounded-full hover:bg-red-700 bg-gray-600 transition-all focus:outline-none ${loading ? "hidden" : ""}`} onClick={delIMG}>
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>
+                                    </button>
+                                    <button className={`loading_dots px-8 py-3 rounded-full hover:bg-gray-800 bg-gray-600 transition-all focus:outline-none disabled:opacity-70 ${loading ? "" : "hidden"}`} disabled={true}>
+                                        ••••••
                                     </button>
                                 </div>
                             </div>
@@ -158,7 +157,7 @@ const Profile = () => {
                                     <div className='px-2 space-y-4'>
                                         <div className='flex justify-center space-x-1'>
                                             <h3 className='text-5xl text-center font-bold py-3' hidden={edit ? true : false}>{user.username}</h3>
-                                            <button title="Edit name" className='hover:bg-gray-800 rounded-full mt-2 h-12 px-4 transition-all focus:outline-none' onClick={handleEdit} hidden={edit ? true : false}>
+                                            <button title="Edit name" className='hover:bg-gray-800 rounded-full mt-2 h-12 px-4 transition-all focus:outline-none disabled:opacity-70' onClick={handleEdit} hidden={edit ? true : false} disabled={loading ? true : false}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                                 </svg>
@@ -185,7 +184,7 @@ const Profile = () => {
                                     </div>
                                 </div>
                                 <div className='flex justify-center mt-4'>
-                                    <button title="Sign out" className='px-8 py-3 rounded-full hover:bg-gray-800 bg-gray-600 transition-all focus:outline-none' onClick={handleLogout}>
+                                    <button title="Sign out" className='px-8 py-3 rounded-full hover:bg-gray-800 bg-gray-600 transition-all focus:outline-none disabled:opacity-70' onClick={handleLogout} disabled={loading ? true : false}>
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                                         </svg>
@@ -229,7 +228,18 @@ const Profile = () => {
                 </div>
             </div>
         </div>
-    ) : "Loading..."
+    ) : (
+        <div className='overflow-hidden z-0'>
+            <div className='z-50 flex flex-col items-center justify-center overflow-auto bg-gray-900 h-screen w-screen'>
+                <h3 className='z-50 loading_dots text-white text-4xl'>
+                    ••••••
+                </h3>
+            </div>
+            <div className='w-0 h-screen fixed top-0 left-0 z-0'>
+                <img className='opacity-30 min-h-max min-w-max ' src={defbg}></img>
+            </div>
+        </div>
+    )
 }
 
 export default Profile
