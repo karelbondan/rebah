30b8efcfa6573fcf4bd5fbf80970745bd354577a
karelbondan <karelbondan@gmail.com>
2022-06-03 04:36:46 +0700

    added styling to loading state

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,10 +1,11 @@
 import { createContext, useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
+import defbg from '../components/items/bg3.jpg'
 
 export const authContext = createContext()
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
@@ -15,9 +16,20 @@ const AuthProvider = ({children}) => {
         });
     }, []);
     if (loading) {
-        return "Loading";
+        return (
+            <div className='overflow-hidden z-0'>
+                <div className='z-50 flex flex-col items-center justify-center overflow-auto bg-gray-900 h-screen w-screen'>
+                    <h3 className='z-50 loading_dots text-white text-4xl'>
+                        ••••••
+                    </h3>
+                </div>
+                <div className='w-0 h-screen fixed top-0 left-0 z-0'>
+                    <img className='opacity-30 min-h-max min-w-max ' src={defbg}></img>
+                </div>
+            </div>
+        );
     }
-    return <authContext.Provider value={{user}}>{children}</authContext.Provider>
+    return <authContext.Provider value={{ user }}>{children}</authContext.Provider>
 }
 
 export default AuthProvider;
