22f081ce8ad26624f9cf2647e88b360aca2adf45
karelbondan <karelbondan@gmail.com>
2022-06-05 04:28:02 +0700

    added 'sending' indicator, more responsive

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Message = ({ handleSubmit, text, setText, setImg, img }) => {
+const Message = ({ handleSubmit, text, setText, setImg, img, sending }) => {
     const uploadIMG = (e) => {
         e.preventDefault()
         let input = document.createElement('input');
@@ -22,15 +22,20 @@ const Message = ({ handleSubmit, text, setText, setImg, img }) => {
 
     return (
         <div className='px-5 py-5 space-y-5 bg-gray-800'>
-            <div className='flex text-gray-500 space-x-5' hidden={img ? false : true}>
-                <button onClick={removeIMG} hidden={img ? false : true}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-                <h3 className='text-gray-400' hidden={img ? false : true}>
-                    {`Attached: ${img.name}`}
-                </h3>
+            <div hidden={img || sending ? false : true}>
+                <div className={`text-center ${sending ? "" : "hidden"}`}>
+                    <p className='take_me_home opacity-50 text-gray-400'>Sending...</p>
+                </div>
+                <div className='flex text-gray-500 space-x-5' >
+                    <button onClick={removeIMG} hidden={img ? false : true}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                    <h3 className='text-gray-400' hidden={img ? false : true}>
+                        {`Attached: ${img.name}`}
+                    </h3>
+                </div>
             </div>
             <div className='flex space-x-5 w-full'>
                 <button className='buton text-gray-500' onClick={uploadIMG}>
