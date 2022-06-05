c8b8beac09149f3fc2706603a19812de8357e6d0
karelbondan <karelbondan@gmail.com>
2022-06-06 02:07:03 +0700

    fixed empty string or bunch of \n counted as valid msgs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -68,11 +68,12 @@ const Home = () => {
       setMessages(messages)
     })
 
+    // update user status (online/offline) in real time
     onSnapshot(doc(db, 'users', user2), docsnap => {
       setChat(docsnap.data())
     })
 
-    // get last message between logged in user and selected user
+    // get latest message between the signed in user and the user they're currently chatting with
     const docsnap = await getDoc(doc(db, 'lastMessage', id))
     if (docsnap.data() && docsnap.data().from !== user1) {
       await updateDoc(doc(db, 'lastMessage', id), {
@@ -80,6 +81,7 @@ const Home = () => {
         unreadCount: 0
       })
     }
+
     // set this here because the useRef() needs time to get the referenced element
     setTimeout(() => {
       setCurrentChat(user.uid)
@@ -88,7 +90,9 @@ const Home = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    if (!text) {
+    // check if text is empty or only contains a bunch of newlines, if true then dont do anything
+    if (!text || /^[\n]+$/.exec(text)) {
+      setText("")
       return;
     }
     setText("")
@@ -105,34 +109,59 @@ const Home = () => {
       url = downloadurl
     }
 
-
-    await addDoc(collection(db, 'messages', id, 'chat'), {
+    // assign the addDoc to a var to be later used to get the auto-generated ID
+    const add_msg = await addDoc(collection(db, 'messages', id, 'chat'), {
       text,
       from: user1,
       to: user2,
       createdAt: Timestamp.fromDate(new Date()),
+      chatID: id,
+      edited: false,
       media: url || ""
     })
 
-    let previous_data;
-
-    try {
-      let q = await getDoc(doc(db, "lastMessage", id))
-      previous_data = q.data()
-      setPreviousData(q.data())
-    } catch (e) {
-      console.log(e)
-    }
-
-    await setDoc(doc(db, "lastMessage", id), {
-      text,
-      from: user1,
-      to: user2,
-      createdAt: Timestamp.fromDate(new Date()),
-      media: url || "",
-      unread: true,
-      unreadCount: previous_data.from === user1 ? previous_data.unreadCount + 1 : 1
+    // update the just pushed document to include a new field (its own ID) to be used as reference later
+    // (mainly for delete and edit message)
+    await updateDoc(doc(db, 'messages', id, 'chat', add_msg.id), {
+      messageID: add_msg.id
     })
+
+    // setting the unread count for the other user to be then showed notification 
+    let previous_data;
+    try {
+      // get doc from firebase, id is current user's uid combined with the recipient's uid
+      let q = await getDoc(doc(db, "lastMessage", id))
+      // set previous data to the doc gotten from the query to be then referenced in the doc updt below
+      previous_data = q.data()
+      // this func below is to inform the useEffect to scroll to bottom of page
+      setPreviousData(q.data())
+      // wait for the update of doc. if this message is from the same user then increment the unread counter,
+      // else reset it to 1 because it means the recipient has read the message
+      await setDoc(doc(db, "lastMessage", id), {
+        text,
+        from: user1,
+        to: user2,
+        createdAt: Timestamp.fromDate(new Date()),
+        media: url || "",
+        messageRefID: add_msg.id,
+        unread: true,
+        unreadCount: previous_data.from === user1 ? previous_data.unreadCount + 1 : 1
+      })
+    } catch (e) {
+      // if this is the first time the sender chats with the receiver then it won't have 'lastMessage' document,
+      // therefore unreadCount just becomes 1, it doesn't matter because the one going to see it is the receiver
+      await setDoc(doc(db, "lastMessage", id), {
+        text,
+        from: user1,
+        to: user2,
+        createdAt: Timestamp.fromDate(new Date()),
+        media: url || "",
+        messageRefID: add_msg.id,
+        unread: true,
+        unreadCount: 1
+      })
+    }
+    // set to false to hide the 'sending' indicator
     setSending(false)
   }
 
