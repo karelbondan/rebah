895a416fca587c06870c941f4e8b249a60aca42b
karelbondan <karelbondan@gmail.com>
2022-06-06 02:03:45 +0700

    changed from input to textarea field

--- src/components/Message.js
+++ src/components/Message.js
@@ -20,6 +20,27 @@ const Message = ({ handleSubmit, text, setText, setImg, img, sending }) => {
         setImg("")
     }
 
+    const handleEdit = (e) => {
+        if (e.shiftKey && e.keyCode === 13) {
+            return
+        } else if (e.keyCode === 13) {
+            handleSubmit(e)
+            const hadeh = document.getElementById("msg_write_area")
+            hadeh.style.height = "";
+        }
+    }
+
+    const handleChange = (e) => {
+        if (e.keyCode === 13) {
+            return
+        } else {
+            setText(e.target.value)
+        }
+        const hadeh = document.getElementById("msg_write_area")
+        hadeh.style.height = "";
+        hadeh.style.height = `${Math.min(hadeh.scrollHeight, 384)}px`;
+    }
+
     return (
         <div className='px-5 py-5 space-y-5 bg-gray-800'>
             <div hidden={img || sending ? false : true}>
@@ -44,7 +65,8 @@ const Message = ({ handleSubmit, text, setText, setImg, img, sending }) => {
                     </svg>
                 </button>
                 <form className='flex space-x-5 justify-center w-full'>
-                    <input className='w-full rounded-xl px-3 py-2 focus:outline-none bg-gray-700 text-gray-300' type='text' placeholder='Enter message' value={text} onChange={(e) => { setText(e.target.value) }}></input>
+                    {/* <input className='w-full rounded-xl px-3 py-2 focus:outline-none bg-gray-700 text-gray-300' type='text' placeholder='Enter message' value={text} onChange={(e) => { setText(e.target.value) }}></input> */}
+                    <textarea id='msg_write_area' className='w-full rounded-xl px-3 py-2 focus:outline-none bg-gray-700 text-gray-300' type='text' placeholder='Enter message' value={text} onChange={handleChange} onKeyDown={handleEdit} />
                     <button className='text-gray-500' onClick={handleSubmit}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
