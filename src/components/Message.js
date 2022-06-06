14ea29d852bf26cc4506d4dd24909983ad591744
karelbondan <karelbondan@gmail.com>
2022-06-06 22:45:03 +0700

    fixed minor bug

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,13 +8,18 @@ const Message = ({ handleSubmit, text, setText, setImg, img, sending }) => {
         input.accept = 'image/*'
         input.onchange = _this => {
             let files = Array.from(input.files)[0];
-            if (files.type !== 'image/jpg' || files.type !== 'image/jpeg' || files.type !== 'image/png') {
+            // if (files.type !== 'image/jpg' || files.type !== 'image/jpeg' || files.type !== 'image/png') {
+            //     alert("Please upload only image formatted file (JPG/PNG)")
+            //     return
+            // }
+            if (/^image\/[\w]+$/.exec(files.type)) {
+                setImg(files)
+                console.log(files)
+                console.log(files.size * 2 ** -10)
+            } else {
                 alert("Please upload only image formatted file (JPG/PNG)")
                 return
             }
-            setImg(files)
-            console.log(files)
-            console.log(files.size * 2 ** -10)
         };
         input.click();
     }
@@ -70,7 +75,7 @@ const Message = ({ handleSubmit, text, setText, setImg, img, sending }) => {
                 </button>
                 <form className='flex space-x-5 justify-center w-full'>
                     {/* <input className='w-full rounded-xl px-3 py-2 focus:outline-none bg-gray-700 text-gray-300' type='text' placeholder='Enter message' value={text} onChange={(e) => { setText(e.target.value) }}></input> */}
-                    <textarea id='msg_write_area' className='w-full rounded-xl px-3 py-2 focus:outline-none bg-gray-700 text-gray-300' type='text' placeholder='Enter message' value={text} onChange={handleChange} onKeyDown={handleEdit} />
+                    <textarea id='msg_write_area' className='w-full rounded-xl px-3 py-2 focus:outline-none bg-gray-700 text-gray-300' type='text' placeholder='Enter message' value={text} onChange={handleChange} onKeyDown={handleEdit} autoFocus />
                     <button className='text-gray-500' onClick={handleSubmit}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
